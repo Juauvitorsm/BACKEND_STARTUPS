00f8664154bb1887e2264f1a1a223e2f51d4d4e7
João Vitor Sousa Martins <jvitor.005@gmail.com>
2025-10-07 12:36:51 -0300

    feat: testes3

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 
-chmod +x ./start.sh
+mkdir -p /app/nltk_data
 
 
-echo "Configurando NLTK..."
-python -c "import nltk; nltk.download('punkt', quiet=True); nltk.download('stopwords', quiet=True); nltk.download('rslp', quiet=True)"
+export NLTK_DATA=/app/nltk_data
+
+
+echo "Forçando download de recursos NLTK para $NLTK_DATA"
+python -c "import nltk; nltk.download('punkt', download_dir='$NLTK_DATA', quiet=True); nltk.download('stopwords', download_dir='$NLTK_DATA', quiet=True); nltk.download('rslp', download_dir='$NLTK_DATA', quiet=True)"
 
 
 echo "Iniciando Uvicorn..."
-exec uvicorn backend.app.main:app --host 0.0.0.0 --port $PORT
+exec uvicorn backend.app.main:app --host 0.0.0.0 --port \$PORT
