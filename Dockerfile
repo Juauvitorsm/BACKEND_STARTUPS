9c3dcac783cb4aaa90a824b5c07949ff3800ffb9
João Vitor Sousa Martins <jvitor.005@gmail.com>
2025-10-13 14:11:09 -0300

    feat: docker cmdd

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,4 @@ RUN mkdir -p ${NLTK_DATA} \
 
 COPY . .
 
-CMD ["python", "backend/app/main.py"]
+CMD ["uvicorn", "backend.app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- backend/app/main.py
+++ backend/app/main.py
@@ -134,5 +134,5 @@ def optimized_search_companies(
     
     return results
 
-if __name__ == "__main__":
-    uvicorn.run("backend.app.main:app", host="127.0.0.1", port=8000, reload=True)
+# # if __name__ == "__main__":
+# #     uvicorn.run("backend.app.main:app", host="127.0.0.1", port=8000, reload=True)
